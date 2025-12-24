b0a0bab1aad496835845c892755eb672438fcc3b
durgadeepa555-sketch <durgadeepa555@gmail.com>
2025-12-24 22:42:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showText() {
+  document.getElementById("textBox").style.display = "block";
+}
+
+
+function showAudio() {
+  document.getElementById("audioBox").style.display = "block";
+}
